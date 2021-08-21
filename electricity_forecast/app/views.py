b7b3b9e090d8d794334e291f6013876db7ce9634
sevrn <sevrn@inbox.ru>
2021-08-21 13:50:24 +0300

    123

--- electricity_forecast/app/views.py
+++ electricity_forecast/app/views.py
@@ -1,10 +1,11 @@
 import requests
 from xml.etree import ElementTree
-from datetime import datetime
+import datetime
+import pandas as pd
+import statsmodels.api as sm
 from django.shortcuts import render
 from app.forms import MainForm
 from app.visualizator import create_nodal_func_plot
-
 
 def index(request):
     """
@@ -15,16 +16,14 @@ def index(request):
     dt_date_from = str(main_form['dt_date_from'].value()).replace('-', '').split(' ')[0]
     dt_date_to = str(main_form['dt_date_to'].value()).replace('-', '').split(' ')[0]
     if request.POST:
-        datetimeobject = datetime.strptime(dt_date_from, '%d.%m.%Y')
+        datetimeobject = datetime.datetime.strptime(dt_date_from, '%d.%m.%Y')
         dt_date_from = datetimeobject.strftime('%Y%m%d')
-        datetimeobject = datetime.strptime(dt_date_to, '%d.%m.%Y')
+        datetimeobject = datetime.datetime.strptime(dt_date_to, '%d.%m.%Y')
         dt_date_to = datetimeobject.strftime('%Y%m%d')
 
     period = int(main_form['period'].value())
     operating_parameter = main_form['operating_parameter'].value()
-    oes = main_form['oes'].value()
-
-    print(dt_date_from, dt_date_to)
+    oes = str(main_form['oes'].value())
 
     str_dpg_code = 'oes'
     r = requests.get(f'https://www.atsenergo.ru/market/stats.xml?date1={dt_date_from}&date2={dt_date_to}&period={period}&type={str_dpg_code}')
@@ -51,9 +50,9 @@ def index(request):
     if period == 1:
         for day in [xmlDict[key] for key in x_data]:
             for item in day:
-                if item[0] == oes:
+                if item[1] == oes:
                     y_data.append(float(item[operating_parameter_index]))
-        x_data = [f'{day} - {hour} ч' for day in x_data for hour in [i for i in range(24)]]
+        x_data = [f'{day} {hour}' for day in x_data for hour in [i for i in range(24)]]
     else:
         for day in [xmlDict[key] for key in x_data]:
             for item in day:
@@ -64,7 +63,25 @@ def index(request):
     for item in MainForm.choices_operating_parameter:
         if item[0] == operating_parameter:
             y_axis = item[1]
-    plot = create_nodal_func_plot(x_data, y_data, f'Динамика исследуемого параметра - {operating_parameter}', 'Дата', y_axis)
+
+    d = {'Date': x_data, 'Value': y_data}
+    df = pd.DataFrame(data=d)
+    df['Date'] = pd.to_datetime(df['Date'], format='%d.%m.%Y %H') if period == 1 else pd.to_datetime(df['Date'], format='%d.%m.%Y')
+    df.set_index('Date', inplace=True)
+
+    date_forecast = dt_date_to
+    forecast = pd.to_datetime(f'{date_forecast}', format='%Y%m%d') + datetime.timedelta(days=2)
+
+    arma_mod30 = sm.tsa.ARMA(df, (3, 0)).fit(disp=False) if period == 1 else sm.tsa.ARMA(df, (1, 0)).fit(disp=False)
+    predict_sunspots = arma_mod30.predict(df.index[-1], forecast, dynamic=True)
+    prediction_x = []
+    for time in predict_sunspots.index.tolist():
+        datetimeobject = datetime.datetime.strptime(str(time).split(' ')[0], '%Y-%m-%d')
+        prediction_x.append(datetimeobject.strftime('%d.%m.%Y'))
+    if period == 1:
+        prediction_x = [f'{day} {hour}' for day in prediction_x for hour in [i for i in range(24)]]
+
+    plot = create_nodal_func_plot(x_data, y_data, prediction_x, predict_sunspots.values.tolist(), f'Динамика исследуемого параметра - {operating_parameter}', 'Дата', y_axis)
 
     return render(request, 'app/index.html', {
         'main_form': main_form,
--- electricity_forecast/app/visualizator.py
+++ electricity_forecast/app/visualizator.py
@@ -2,7 +2,7 @@ from plotly.offline import plot
 from plotly.graph_objects import Scattergl, Layout, Figure
 
 
-def create_nodal_func_plot(x_list: list, y_list: list, name: str, xaxis: str, yaxis: str):
+def create_nodal_func_plot(x_list: list, y_list: list, predict_x: list, predict_y: list, name: str, xaxis: str, yaxis: str):
     """
     ## Функция построения графика динамики параметров стоимости электричества
 
@@ -10,6 +10,8 @@ def create_nodal_func_plot(x_list: list, y_list: list, name: str, xaxis: str, ya
     ----------
     :param x_list: список с данными по x
     :param y_list: список с данными по y
+    :param predict_x: список с прогнозными данными по x
+    :param predict_y: список с прогнозными данными по y
     :param name: анотация к trace
     :param xaxis: подпись оси y
     :param yaxis: подпись оси y
@@ -22,9 +24,12 @@ def create_nodal_func_plot(x_list: list, y_list: list, name: str, xaxis: str, ya
     data.append(Scattergl(y=y_list, x=x_list, mode='lines',
                           line={'dash': 'solid', 'color': '#AF479D'},
                           name=name))
+    data.append(Scattergl(y=predict_y, x=predict_x, mode='lines',
+                          line={'dash': 'solid', 'color': '#60B1DC'},
+                          name='Прогноз'))
     layout = Layout(
         xaxis={'title': xaxis},
-        yaxis={'title': yaxis}, width=800, height=500, legend=dict(orientation="h"),
+        yaxis={'title': yaxis}, width=800, height=600, legend=dict(orientation="h", y=1.1),
         paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', )
     figure = Figure(data=data, layout=layout)
     figure.update_xaxes(linewidth=2, linecolor='#A6A8AB', gridcolor='#A6A8AB')
